01823b2572c5d98e053edcd9e938e0ceb9182d2d
Hongyanlee0614 <hongyanlee0614@gmail.com>
2021-01-25 16:33:02 +0800

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--bag image -->
+  <div id="bag" :class="{burst: ended}">
+
+  </div>
+  <!--bag health -->
+  <div id="bag-health">
+    <div :style="{width: health+'%'}"></div>
+  </div>
+  <!--game control -->
+  <div id="controls">
+    <button @click="punch" v-if="!ended">Punch</button>
+    <div class="divider"></div>
+    <button @click="restart">Restart</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      health: 100,
+      ended: false
+    }
+  },
+  methods: {
+    punch(){
+      this.health -= 10
+      if (this.health <= 0){
+        this.ended = true
+      }
+    },
+    restart(){
+      this.health = 100
+      this.ended = false
+    }
   }
 }
 </script>
@@ -22,5 +48,46 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#bag{
+  width: 200px;
+  height: 500px;
+  margin: 0 auto;
+  background: url('./assets/bag.png') center no-repeat;
+  background-size: 80%
+}
+#bag.burst{
+  background: url('./assets/bag-burst.png') center no-repeat;
+  background-size: 80%
+}
+#bag-health{
+  width: 200px;
+  border: 2px solid #000;
+  margin: 0 auto 20px auto;
+}
+#bag-health div{
+  height: 20px;
+  background: crimson;
+}
+#controls{
+  width: 120px;
+  margin: 0 auto;
+}
+button{
+  color: black;
+  transition-duration: 0.4s;
+  border-radius: 10px;
+  padding: 6px;
+}
+
+button:hover {
+  background-color: black;
+  color: white;
+  border: 2px solid white;
+}
+.divider{
+  width: 5px;
+  height: auto;
+  display: inline-block;
 }
 </style>
